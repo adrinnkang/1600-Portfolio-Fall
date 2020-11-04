f698387be5d625681e1ea09d916c2ea50e4d67b1
Adrinn Kang <adrinn.kang@gmail.com>
2020-11-03 21:29:25 -0700

    Looped through films and people

---
+++ star-wars-api/main.js
@@ -0,0 +1,20 @@
+import { films } from './data/films.js'
+import { people } from './data/people.js'
+
+// console.log(document.body.children)
+
+const main = document.querySelector('main')
+
+/* films.forEach(film => {
+    console.log(film.title)
+    let newTitle = main.appendChild(document.createElement("h1"))
+    newTitle.textContent = film.title
+}) */
+
+people.forEach(person => {
+    console.log(person.name)
+    let personImg = document.createElement('img')
+    personImg.src = "https://starwars-visualguide.com/assets/img/characters/10.jpg"
+    main.appendChild(personImg)
+
+})
