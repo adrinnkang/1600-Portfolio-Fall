ef430d505073560bf00972cfadef6d3ed34fee97
Adrinn Kang <adrinn.kang@gmail.com>
2020-11-10 16:33:07 -0700

    Added iFrame

--- tabs.js
+++ tabs.js
@@ -2,33 +2,25 @@ const charTab = document.querySelector('.characters')
 const filmsTab = document.querySelector('.films')
 const shipsTab = document.querySelector('.starships')
 
-const charContent = document.querySelector('.charContent')
-const filmContent = document.querySelector('.filmContent')
-const starshipContent = document.querySelector('.starshipContent')
+const mainFrame = document.querySelector('.mainFrame')
 
 charTab.addEventListener('click', () => {
     charTab.classList.toggle("is-active")
     filmsTab.classList.remove("is-active")
     shipsTab.classList.remove("is-active")
-    charContent.style.display = 'block'
-    filmContent.style.display = 'none'
-    starshipContent.style.display = 'none'
+    mainFrame.src = "./star-wars-characters/index.html"
 })
 
 filmsTab.addEventListener('click', () => {
     filmsTab.classList.toggle("is-active")
     shipsTab.classList.remove("is-active")
     charTab.classList.remove("is-active")
-    filmContent.style.display = 'block'
-    charContent.style.display = 'none'
-    starshipContent.style.display = 'none'
+    mainFrame.src = "./star-wars-films/index.html"
 })
 
 shipsTab.addEventListener('click', () => {
     shipsTab.classList.toggle("is-active")
     charTab.classList.remove("is-active")
     filmsTab.classList.remove("is-active")
-    starshipContent.style.display = 'block'
-    charContent.style.display = 'none'
-    filmContent.style.display = 'none'
+    mainFrame.src = "./star-wars-starships/index.html"
 })
