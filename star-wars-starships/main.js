69f76c16741781f2ab3b4eb90606fe8e475ce303
Adrinn Kang <adrinn.kang@gmail.com>
2020-11-13 19:42:38 -0700

    Fixed films captions and starfield

--- star-wars-starships/main.js
+++ star-wars-starships/main.js
@@ -58,6 +58,7 @@ function addStarField(element, numStars) {
         star.style.setProperty('width', '2px')
         star.style.setProperty('height', '2px')
         star.style.setProperty('background-color', 'white')
+        //star.style.setProperty('z-index', '-1')
         let xy = getRandomPosition()
         star.style.left = `${xy[0]}px`
         star.style.top =`${xy[1]}px`
