154aa504ac6fac6f483e4a07cd6c6c0d2b78b546
Adrinn Kang <adrinn.kang@gmail.com>
2020-11-20 22:58:14 -0700

    Started adding async function

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,23 +1,33 @@
-const pokeGrid = document.querySelector('.pokemonGrid')
 
-async function loadData() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
-    const data = await response.json()
-    populatePokePage(data)
-}
-
-function populatePokePage(data) {
-    const allPokemon = data.results
-    for (const pokemon of allPokemon) {
-        let pokeCard = document.createElement('div')
-        pokeCard.className = 'pokecard'
-        let pokeName = document.createElement('h3')
-        pokeName.textContent = pokemon.name
-
-        pokeCard.appendChild(pokeName)
-        pokeGrid.appendChild(pokeCard)
+// Reusable async function to fetch data from the provided url
+async function getAPIData(url) {
+    try  {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
     }
 }
 
-loadData()
+// Now, use the async getAPIData function
+function loadPage() {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon`).then
+    (async (data) => {
+        for (const pokemon of data.results) {
+            await getAPIData(pokemon.url).then((pokeData) => {
+                populatePokeCard(pokeData)
+            })
+        }
+    })
+
+}
+
+const pokeGrid = document.querySelector('.pokemonGrid')
+
+function populatePokeCard(singlePokemon) {
+    console.log(singlePokemon)
+}
+
+loadPage()
 
